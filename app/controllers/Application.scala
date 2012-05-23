afa56e57f712686f121b43eec6cc345d607cff95
steppenwells <steppenwells@gmail.com>
2012-05-23 17:36:19 +0100

    debug output for nyt

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,7 +5,7 @@ import libs.json.Reads
 import play.api.mvc._
 import org.joda.time.DateTime
 import play.api.libs.json.Json._
-import model.{AbstractStory, GuImporter}
+import model.{NYTStoryCollector, AbstractStory, GuImporter}
 
 object Application extends Controller {
   implicit val reads = Reads
@@ -17,6 +17,11 @@ object Application extends Controller {
   def stats_gu = Action {
 
     Ok(toJson(GuImporter.storiesSince(DateTime.now.minusHours(24)).await.get))
+  }
+
+  def stats_nyt = Action {
+
+    Ok(toJson(NYTStoryCollector.storiesSince(DateTime.now.minusHours(24)).await.get))
   }
   
 }

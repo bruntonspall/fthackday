707f3dfb3f791187a1e2ffea99ea9b506e8b9f05
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2012-05-23 16:37:12 +0100

    Bring in the tags as well
    Now get the tags and clear them down.

--- app/model/GuImporter.scala
+++ app/model/GuImporter.scala
@@ -5,14 +5,14 @@ import org.joda.time.DateTime
 import org.joda.time.format.{ISODateTimeFormat, DateTimeFormat}
 
 object GuImporter {
-  def storiesSince(dt: DateTime) = WS.url("http://content.guardianapis.com/search.json").get().map
-  { response =>
-    (response.json \ "response" \ "results").asInstanceOf[JsArray].value.map
-    { result =>
-      AbstractStory(
-        (result \ "webTitle").as[String],
-        Nil,
-        DateTime.parse((result \ "webPublicationDate").as[String], ISODateTimeFormat.dateTimeNoMillis))
+  def storiesSince(dt: DateTime) = WS.url("http://content.guardianapis.com/search.json?show-tags=keyword").get().map {
+    response =>
+    (response.json \ "response" \ "results").asInstanceOf[JsArray].value.map {
+      result =>
+        AbstractStory(
+          (result \ "webTitle").as[String],
+          (result \ "tags" \\ "webTitle").map(_.as[String]),
+          DateTime.parse((result \ "webPublicationDate").as[String], ISODateTimeFormat.dateTimeNoMillis))
     }
   }
 }

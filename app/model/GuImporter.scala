12ebae50cebd840f05f6cf8fb73921d444f81422
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2012-05-23 16:11:42 +0100

    GuImporter step 1 [Broken]

--- app/model/GuImporter.scala
+++ app/model/GuImporter.scala
@@ -1,5 +1,13 @@
 package model
+import play.api.libs.ws.WS
+import play.api.libs.json.JsArray
+import org.joda.time.DateTime
+import org.joda.time.format.{ISODateTimeFormat, DateTimeFormat}
 
 class GuImporter {
-
+  val json = WS.url("http://content.guardianapis.com/search.json").get().map{ response =>
+    (response.json \ "response" \ "results").asInstanceOf[JsArray].value.map { result =>
+      AbstractStory(result \ "webTitle" toString(), Nil, DateTime.parse(result \ "webPublicationDate" toString(), ISODateTimeFormat.dateTimeNoMillis))
+    }
+  }
 }

71eefcc21345cc71fe95805a998eac69f016030b
Philip Wills <philip.wills@guardian.co.uk>
2012-05-23 17:39:27 +0100

    Get tags from the FT content API

--- app/model/FTStoryCollector.scala
+++ app/model/FTStoryCollector.scala
@@ -4,6 +4,7 @@ import org.joda.time.DateTime
 import play.api.libs.ws.WS
 import org.joda.time.format.ISODateTimeFormat
 import play.api.libs.json.{JsValue, Reads, JsArray}
+import play.api.libs.concurrent.Promise
 
 object FTStoryCollector extends StoryImporter {
   def storiesSince(dt: DateTime) = {
@@ -11,16 +12,19 @@ object FTStoryCollector extends StoryImporter {
 
     WS.url("http://api.ft.com/content/search/v1?apiKey=8f4a8b83f48c4eecf9b6b64c90a4451b").post(
       """{ "queryString": "-zxxzxz AND (lastPublishDateTime:>%s)"}""".format(dateFormat.print(dt))
-    ) map {
-      response =>
-        (response.json \ "results" \\ "results").flatMap(_.asInstanceOf[JsArray].value map {
-          result : JsValue =>
+    ) flatMap { response =>
+      Promise.sequence((response.json \ "results" \\ "results").flatMap(_.asInstanceOf[JsArray].value map {
+        result : JsValue =>
+          val contentApiUrl = (result \ "apiUrl").as[String]
+
+          WS.url("%s?apiKey=8f4a8b83f48c4eecf9b6b64c90a4451b".format(contentApiUrl)).get() map { itemResponse =>
             AbstractStory(
               (result \ "title" \ "title").as[String],
-              Nil,
+              (itemResponse.json \ "item" \ "metadata" \ "tags" \\ "name").map(_.as[String]),
               parseDate((result \ "lifecycle" \ "lastPublishDateTime").as[String])
             )
-        })
+          }
+      }))
     }
   }
 }

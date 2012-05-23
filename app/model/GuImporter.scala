f9c220bb3d1a4a81f8dbcd87e0c6868c2c9385a1
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2012-05-23 16:27:23 +0100

    Change to match phils work
    Also make it actually work

--- app/model/GuImporter.scala
+++ app/model/GuImporter.scala
@@ -4,10 +4,15 @@ import play.api.libs.json.JsArray
 import org.joda.time.DateTime
 import org.joda.time.format.{ISODateTimeFormat, DateTimeFormat}
 
-class GuImporter {
-  val json = WS.url("http://content.guardianapis.com/search.json").get().map{ response =>
-    (response.json \ "response" \ "results").asInstanceOf[JsArray].value.map { result =>
-      AbstractStory(result \ "webTitle" toString(), Nil, DateTime.parse(result \ "webPublicationDate" toString(), ISODateTimeFormat.dateTimeNoMillis))
+object GuImporter {
+  def storiesSince(dt: DateTime) = WS.url("http://content.guardianapis.com/search.json").get().map
+  { response =>
+    (response.json \ "response" \ "results").asInstanceOf[JsArray].value.map
+    { result =>
+      AbstractStory(
+        (result \ "webTitle").as[String],
+        Nil,
+        DateTime.parse((result \ "webPublicationDate").as[String], ISODateTimeFormat.dateTimeNoMillis))
     }
   }
 }

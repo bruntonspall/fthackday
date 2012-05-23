345a935a76a14330d3986b74e8f1707a82b3fcb1
Philip Wills <philip.wills@guardian.co.uk>
2012-05-23 19:31:19 +0100

    Cache the content API calls
    Not pretty.

--- app/model/FTStoryCollector.scala
+++ app/model/FTStoryCollector.scala
@@ -5,8 +5,11 @@ import play.api.libs.ws.WS
 import org.joda.time.format.ISODateTimeFormat
 import play.api.libs.json.{JsValue, Reads, JsArray}
 import play.api.libs.concurrent.Promise
+import play.api.cache.Cache
 
 object FTStoryCollector extends StoryImporter {
+  import play.api.Play.current
+
   def storiesSince(dt: DateTime) = {
     implicit val reads = Reads
 
@@ -17,16 +20,25 @@ object FTStoryCollector extends StoryImporter {
         result : JsValue =>
           val contentApiUrl = (result \ "apiUrl").as[String]
 
-          WS.url("%s?apiKey=8f4a8b83f48c4eecf9b6b64c90a4451b".format(contentApiUrl)).get() map { itemResponse =>
-            itemResponse.status match {
-              case 200 => Some(AbstractStory(
-                (result \ "title" \ "title").as[String],
-                (itemResponse.json \ "item" \ "metadata" \ "tags" \\ "name").map(_.as[String]),
-                parseDate((result \ "lifecycle" \ "lastPublishDateTime").as[String]),
-                "FT"
-              ))
-              case _ => None
+          Cache.getAs[AbstractStory](contentApiUrl) match {
+            case None => {
+              WS.url("%s?apiKey=8f4a8b83f48c4eecf9b6b64c90a4451b".format(contentApiUrl)).get() map { itemResponse =>
+                itemResponse.status match {
+                  case 200 => {
+                    val story = Some(AbstractStory(
+                      (result \ "title" \ "title").as[String],
+                      (itemResponse.json \ "item" \ "metadata" \ "tags" \\ "name").map(_.as[String]),
+                      parseDate((result \ "lifecycle" \ "lastPublishDateTime").as[String]),
+                      "FT"
+                    ))
+                    Cache.set(contentApiUrl, story)
+                    story
+                  }
+                  case _ => None
+                }
+              }
             }
+            case Some(x) => Promise.pure(Some(x))
           }
       })) map (_.flatten)
     }

d8a7b5ec77771cc88615f76d9b776c80d2b52661
steppenwells <steppenwells@gmail.com>
2012-05-24 12:19:04 +0100

    wip word cloud

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -58,12 +58,12 @@ object Application extends Controller {
   }}
 
 
-  val stopwords = """i|me|my|myself|we|us|our|ours|ourselves|you|your|yours|yourself|yourselves|he|him|his|himself|she|her|hers|herself|it|its|itself|they|them|their|theirs|themselves|what|which|who|whom|whose|this|that|these|those|am|is|are|was|were|be|been|being|have|has|had|having|do|does|did|doing|will|would|should|can|could|ought|i'm|you're|he's|she's|it's|we're|they're|i've|you've|we've|they've|i'd|you'd|he'd|she'd|we'd|they'd|i'll|you'll|he'll|she'll|we'll|they'll|isn't|aren't|wasn't|weren't|hasn't|haven't|hadn't|doesn't|don't|didn't|won't|wouldn't|shan't|shouldn't|can't|cannot|couldn't|mustn't|let's|that's|who's|what's|here's|there's|when's|where's|why's|how's|a|an|the|and|but|if|or|because|as|until|while|of|at|by|for|with|about|against|between|into|through|during|before|after|above|below|to|from|up|upon|down|in|out|on|off|over|under|again|further|then|once|here|there|when|where|why|how|all|any|both|each|few|more|most|other|some|such|no|nor|not|only|own|same|so|than|too|very|say|says|said|shall"""
+  val stopwords = """-|i|me|my|myself|we|us|our|ours|ourselves|you|your|yours|yourself|yourselves|he|him|his|himself|she|her|hers|herself|it|its|itself|they|them|their|theirs|themselves|what|which|who|whom|whose|this|that|these|those|am|is|are|was|were|be|been|being|have|has|had|having|do|does|did|doing|will|would|should|can|could|ought|i'm|you're|he's|she's|it's|we're|they're|i've|you've|we've|they've|i'd|you'd|he'd|she'd|we'd|they'd|i'll|you'll|he'll|she'll|we'll|they'll|isn't|aren't|wasn't|weren't|hasn't|haven't|hadn't|doesn't|don't|didn't|won't|wouldn't|shan't|shouldn't|can't|cannot|couldn't|mustn't|let's|that's|who's|what's|here's|there's|when's|where's|why's|how's|a|an|the|and|but|if|or|because|as|until|while|of|at|by|for|with|about|against|between|into|through|during|before|after|above|below|to|from|up|upon|down|in|out|on|off|over|under|again|further|then|once|here|there|when|where|why|how|all|any|both|each|few|more|most|other|some|such|no|nor|not|only|own|same|so|than|too|very|say|says|said|shall"""
 
   def allWords = Action {
     val headlineWords = allStories map { stories =>
       val allTheWords = stories.flatMap {s => s.headline.split(" ")}
-      val interestingWords = allTheWords.filterNot(w => stopwords.contains(w))
+      val interestingWords = allTheWords.filterNot(w => stopwords.contains(w.toLowerCase))
 
       val wordsWithCount: Map[String, Int] = interestingWords.foldLeft(Map[String, Int]()){ (m: Map[String, Int], s: String) =>
         m + (s -> (m.getOrElse(s, 0) + 1))
@@ -72,8 +72,12 @@ object Application extends Controller {
       wordsWithCount.keys.toList.sortBy(s => wordsWithCount.get(s)).reverse
     }
     Async {
-      headlineWords map {w => Ok(w.mkString(", "))}
+      headlineWords map {w => Ok(toJson(w))}
     }
+  }
+
+  def wordCloud = Action {
+    Ok(views.html.cloud())
   }
 
   def colour_for(source: String):String = source match {

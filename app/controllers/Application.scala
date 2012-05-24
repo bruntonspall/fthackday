6629aff8a425812a303fad1558cbb110e9a29090
steppenwells <steppenwells@gmail.com>
2012-05-24 11:25:08 +0100

    adds all words endpoint

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -54,7 +54,21 @@ object Application extends Controller {
     WPStoryCollector.storiesSince(DateTime.now.minusHours(24)) map (x => Ok(toJson(x)))
   }}
 
-//  def allWords = Action { Async {
-//
-//  }}
+  val stopwords = """i|me|my|myself|we|us|our|ours|ourselves|you|your|yours|yourself|yourselves|he|him|his|himself|she|her|hers|herself|it|its|itself|they|them|their|theirs|themselves|what|which|who|whom|whose|this|that|these|those|am|is|are|was|were|be|been|being|have|has|had|having|do|does|did|doing|will|would|should|can|could|ought|i'm|you're|he's|she's|it's|we're|they're|i've|you've|we've|they've|i'd|you'd|he'd|she'd|we'd|they'd|i'll|you'll|he'll|she'll|we'll|they'll|isn't|aren't|wasn't|weren't|hasn't|haven't|hadn't|doesn't|don't|didn't|won't|wouldn't|shan't|shouldn't|can't|cannot|couldn't|mustn't|let's|that's|who's|what's|here's|there's|when's|where's|why's|how's|a|an|the|and|but|if|or|because|as|until|while|of|at|by|for|with|about|against|between|into|through|during|before|after|above|below|to|from|up|upon|down|in|out|on|off|over|under|again|further|then|once|here|there|when|where|why|how|all|any|both|each|few|more|most|other|some|such|no|nor|not|only|own|same|so|than|too|very|say|says|said|shall"""
+
+  def allWords = Action {
+    val headlineWords = allStories map { stories =>
+      val allTheWords = stories.flatMap {s => s.headline.split(" ")}
+      val interestingWords = allTheWords.filterNot(w => stopwords.contains(w))
+
+      val wordsWithCount: Map[String, Int] = interestingWords.foldLeft(Map[String, Int]()){ (m: Map[String, Int], s: String) =>
+        m + (s -> (m.getOrElse(s, 0) + 1))
+      }
+
+      wordsWithCount.keys.toList.sortBy(s => wordsWithCount.get(s)).reverse
+    }
+    Async {
+      headlineWords map {w => Ok(w.mkString(", "))}
+    }
+  }
 }
